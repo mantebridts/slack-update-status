e065d3530c62e1aad211d5b631a577bfed2dfde2
Mante Bridts <mantebridts@gmail.com>
2017-11-30 17:59:55 +0100

    cleanup, added ontheroad, added comments

--- lib/slack_wifi_status/status_from_wifi.rb
+++ lib/slack_wifi_status/status_from_wifi.rb
@@ -1,22 +1,56 @@
 module SlackWifiStatus
   class StatusFromWifi
+
+    ##
+    # Get the current connected ssid
+    ##
+
     def self.ssid
       @ssid ||= `/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`.chomp
     end
 
-    def self.home_ssid?
-      home_ssids.include?(ssid)
+    ##
+    # Is the connected ssid in the ssid-list from a place?
+    ##
+
+    def self.kontich_ssid?
+      SlackWifiStatus::Config.config['kontich_ssids'].include?(ssid)
     end
 
-    def self.home_ssids
-      SlackWifiStatus::Config.config['home_ssids']
+    def self.gent_ssid?
+      SlackWifiStatus::Config.config['gent_ssids'].include?(ssid)
     end
+
+    def self.antwerpen_ssid?
+      SlackWifiStatus::Config.config['antwerpen_ssids'].include?(ssid)
+    end
+
+    def self.home_ssid?
+      SlackWifiStatus::Config.config['home_ssids'].include?(ssid)
+    end
+
+    def self.ontheroad_ssid?
+      puts SlackWifiStatus::Config.config['ontheroad_ssids']
+      SlackWifiStatus::Config.config['ontheroad_ssids'].include?(ssid)
+    end
+
+    ##
+    # Check wich ssid is set
+    ##
 
     def self.status_hash
-      if ssid.empty? || home_ssid?
-        { message: 'At Home', emoji: ':house_with_garden:' }
+      if kontich_ssid?
+        { message: '', emoji: ':kontich:' }
+      elsif gent_ssid?
+        { message: '', emoji: ':gent:' }
+      elsif antwerpen_ssid?
+        { message: '', emoji: ':antwerp:' }
+      elsif home_ssid?
+        { message: '', emoji: ':house:' }
+      elsif ontheroad_ssid?
+        { message: '', emoji: '' }
       else
-        { message: 'At a coffee shop', emoji: ':coffee:' }
+        { message: '', emoji: '' }
       end
     end
 
