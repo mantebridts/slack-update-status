78967dae54e6c5d094e089f98b2b3d146b4b5bcb
Koen <koen@beformed.be>
2018-04-10 16:40:46 +0200

    Removed unnecessary variable
    Optimalisation, optimalisation, optimalisation.

--- lib/slack_location_status/status_from_location.rb
+++ lib/slack_location_status/status_from_location.rb
@@ -1,19 +1,18 @@
 module SlackLocationStatus
   class StatusFromLocation
     def self.location
-      @location = `/usr/local/bin/CoreLocationCLI -format "%address"`
+      @location ||= `/usr/local/bin/CoreLocationCLI -format "%address"`
     end
 
     def self.set_status
       config = SlackLocationStatus::Config.config
-      loc = self.location
-      puts "Location used: " + loc
+      puts "Location used: " + self.location
       puts "---------"
       validated = false
       for config_location in config.keys.sort
         if !config[config_location]["address"].nil?
           for possible_match in config[config_location]["address"]
-            if loc.include?(possible_match)
+            if self.location.include?(possible_match)
               validated = true
               puts "Location " + possible_match + " found in address!"
               SlackLocationStatus::UpdateStatus.post({message: config[config_location]["slack"]["message"], emoji: config[config_location]["slack"]["emoji"]})
