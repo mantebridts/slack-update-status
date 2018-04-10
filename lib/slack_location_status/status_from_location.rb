5974181b8b6764ab268a9a32c5482aa1574a4567
Koen <koen@beformed.be>
2018-04-10 16:33:18 +0200

    Updated Terminal command
    Can’t use path-links in Ruby, so it seems, so fixed it with the perfect path!

--- lib/slack_location_status/status_from_location.rb
+++ lib/slack_location_status/status_from_location.rb
@@ -1,18 +1,25 @@
 module SlackLocationStatus
   class StatusFromLocation
     def self.location
-      @location = `CoreLocationCLI -format "%address"`
+      @location = `/usr/local/bin/CoreLocationCLI -format "%address"`
     end
 
     def self.set_status
       config = SlackLocationStatus::Config.config
       loc = self.location
+      puts "Location used: " + loc
+      puts "---------"
       validated = false
-      for key in config.keys.sort
-        if !config[key]["address"].nil?
-          if loc.include?(config[key]["address"][0])
-            validated = true
-            SlackLocationStatus::UpdateStatus.post({message: config[key]["slack"]["message"], emoji: config[key]["slack"]["emoji"]})
+      for config_location in config.keys.sort
+        if !config[config_location]["address"].nil?
+          for possible_match in config[config_location]["address"]
+            if loc.include?(possible_match)
+              validated = true
+              puts "Location " + possible_match + " found in address!"
+              SlackLocationStatus::UpdateStatus.post({message: config[config_location]["slack"]["message"], emoji: config[config_location]["slack"]["emoji"]})
+            else
+              puts "No match for " + possible_match
+            end
           end
         end
       end
