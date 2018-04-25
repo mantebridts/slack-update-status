4090e9f3b8274a6eedae70e178ab612c3c147413
Koen <koen@beformed.be>
2018-04-25 22:41:38 +0200

    SSL reactivated
    And correct URL added

--- local-cron/lib/slack_location_status/update_status.rb
+++ local-cron/lib/slack_location_status/update_status.rb
@@ -6,13 +6,13 @@ module SlackLocationStatus
   # Set the emoji and status text on Slack
   class UpdateStatus
     def self.post(message: '', emoji: '')
-      uri = URI.parse('http://localhost:3000/location')
+      uri = URI.parse('https://api.nightknight.be/location')
 
       header = {'Content-Type': 'application/json'}
       payload = { token: SlackLocationStatus::Config.config['oauth_key'], status: { status_text: message, status_emoji: emoji } }
 
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = false
+      http.use_ssl = true
       request = Net::HTTP::Post.new(uri.request_uri, header)
       request.body = payload.to_json
 
