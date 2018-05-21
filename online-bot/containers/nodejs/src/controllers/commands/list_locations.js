9201ebfa2258b2509b79073482d8fdda5ea89ef7
Mante Bridts <mantebridts@gmail.com>
2018-05-22 00:16:13 +0200

    cleanup format default message

--- online-bot/containers/nodejs/src/controllers/commands/list_locations.js
+++ online-bot/containers/nodejs/src/controllers/commands/list_locations.js
@@ -9,27 +9,27 @@ module.exports = {
 			models.Location.find({user_id: user.user_id}, "name regex status", function(err, locations){
 				if(locations.length == 0){
 					var response = {
-					    "attachments": [
-					        {
-					            "title": "Error: 404",
-					            "text": "No registered locations found :cry:",
-					            "footer": "Sneaky Earl Enterprises",
-					            "footer_icon": "https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-05-10/360767394912_20ddf9c8466bea4c62be_96.png",
-					            "color": "#d40201"
-					        }
-					    ]
+						"attachments": [
+							{
+								"title": "Error: 404",
+								"text": "No registered locations found :cry:",
+								"footer": "Sneaky Earl Enterprises",
+								"footer_icon": "https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-05-10/360767394912_20ddf9c8466bea4c62be_96.png",
+								"color": "#d40201"
+							}
+						]
 					};
 					reject(response);
 				}else{
 					var response = {
 						"text": "A gorgeous list of your locations :sparkles:",
-					    "attachments": []
+						"attachments": []
 					};
 					for(var i=0;i < locations.length;i++){
 						var attachment = {
-							"title": locations[i]['name'], 
+							"title": locations[i]['name'],
 							"callback_id": "location.delete." + locations[i]._id,
-            				"attachment_type": "default",
+							"attachment_type": "default",
 							"fields": [
 								{
 									title: "Matches",
@@ -43,35 +43,35 @@ module.exports = {
 								}
 							],
 							"actions": [
-				                {
-				                    "name": "delete",
-				                    "text": "Delete location",
-				                    "style": "danger",
-				                    "type": "button",
-				                    "value": "true",
-				                    "confirm": {
-				                        "title": "Are you sure?",
-				                        "text": "I mean, really sure?",
-				                        "ok_text": "Yes",
-				                        "dismiss_text": "No"
-				                    }
-				                }
-				            ]
+								{
+									"name": "delete",
+									"text": "Delete location",
+									"style": "danger",
+									"type": "button",
+									"value": "true",
+									"confirm": {
+										"title": "Are you sure?",
+										"text": "I mean, really sure?",
+										"ok_text": "Yes",
+										"dismiss_text": "No"
+									}
+								}
+							]
 						};
 						for(var j=0; j < locations[i]['regex'].length; j++){
 							attachment.fields[0]['value'] += "`- " + locations[i]['regex'][j] + "`\n";
 						}
 
 						response.attachments.push(attachment);
-					}	
+					}
 					response.attachments.push({
 						"text": "",
-			            "footer": "Sneaky Earl Enterprises",
-			            "footer_icon": "https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-05-10/360767394912_20ddf9c8466bea4c62be_96.png",
-			        });
+						"footer": "Sneaky Earl Enterprises",
+						"footer_icon": "https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2018-05-10/360767394912_20ddf9c8466bea4c62be_96.png",
+					});
 					resolve(response);
 				}
 			});
-  		});
+		});
 	}
 };
