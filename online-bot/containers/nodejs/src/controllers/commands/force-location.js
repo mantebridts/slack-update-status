d931f3e8204615fcee3b646d488ff43050d32cca
Koen <koen@beformed.be>
2018-05-28 23:29:36 +0200

    Vertaling force-location

--- online-bot/containers/nodejs/src/controllers/commands/force-location.js
+++ online-bot/containers/nodejs/src/controllers/commands/force-location.js
@@ -4,19 +4,25 @@ const models = require('./../../models/index');
 
 var forceLocation = new Command("force-location");
 
-forceLocation.addStep(/^force/i, {text: "Okay, where would you like to appear?"}, async function(message, user){
+forceLocation.addStep(/^force/i, {text: "Okay, waar wil je graag getoond worden?\n_(Antwoord met 'undo' op deze vraag om je geforceerde status weer te verwijderen)_"}, async function(message, user){
 	return new Promise(function(resolve, reject) {
-		//Fetch location from db
-		models.Location.findOne({name: message.text, user_id: user.user_id}, "_id name status", function(err, location){
-			if(!location){
-				reject("No location with this name found.. :cry:");
-			}else{
-            	models.User.findOneAndUpdate({user_id: user.user_id}, {location: { address: "(Forced) " + location.name, _id: location._id}, last_active: new Date()}, function(err, u){
-					resolve("Location forced to " + location.name);
-					_updateSlackStatus(u.token, location.status);
-            	});
-			}
-		});
+		if(message.text == "undo"){
+        	models.User.findOneAndUpdate({user_id: user.user_id}, {location: null, last_active: new Date()}, function(err, u){
+				resolve("Forced location verwijderd. Geef het even, en je bent weer ergens!");
+        	});
+		}else{		
+			//Fetch location from db
+			models.Location.findOne({name: message.text, user_id: user.user_id}, "_id name status", function(err, location){
+				if(!location){
+					reject("Geen locatie met deze naam gevonden.. :cry:");
+				}else{
+	            	models.User.findOneAndUpdate({user_id: user.user_id}, {location: { address: "(Forced) " + location.name, _id: location._id}, last_active: new Date()}, function(err, u){
+						resolve("Locatie geforceerd naar " + location.name);
+						_updateSlackStatus(u.token, location.status);
+	            	});
+				}
+			});
+		}
 	});
 });
 
